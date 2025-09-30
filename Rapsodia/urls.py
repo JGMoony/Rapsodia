b7e4d0630bf40bf0f73321d80db95df9750cbf5d
lizeth93993 <jlizeth790@gmail.com>
2025-09-30 17:53:02 -0500

    panel de administrador funcional

--- Rapsodia/urls.py
+++ Rapsodia/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('reservations/', include('reservations.urls')),
     path('administration/', include('administration.urls')),
+    
 ]
