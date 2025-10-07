fce8544860c60b1bb05f88853ffe89c3ac1030ea
Mr.Mostacho <133924865+MrMostacho04@users.noreply.github.com>
2025-10-06 19:03:09 -0500

    Preparación inicial para despliegue en Heroku
    actualización requirements.txt
    
    Actualiza el archivo requirements.txt para reflejar cambios en las dependencias del proyecto.
    
    se refactorizo la autenticación de usuarios y las vistas para mejorar la funcionalidad; implementar la integración con Google OAuth2 y ajustes los campos del modelo de usuario.

--- Rapsodia/urls.py
+++ Rapsodia/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from users.views import base_view, login_view, logout_view, registro_view
+from users.views import home, login_view, logout_view, registro_view
 
 urlpatterns = [
     path('', login_view, name='login'),
-    path('home/', base_view, name='base'),
+    path('home/', home, name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('reservations/', include('reservations.urls')),
